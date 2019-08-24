76f163403f1f2304c7da6bbf9bc1a716c5cdbf78
t-cheepeng <tcp97@hotmail.com>
2019-08-24 21:10:15 +0800

    Completed extension A-JUnit and A-Gradle.

---
+++ src/test/java/ToDoTest.java
@@ -0,0 +1,19 @@
+import com.chee.model.ToDo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+
+    private ToDo emptyTodo = new ToDo("");
+
+    @Test
+    public void getSaveString_emptyDescription_correctSaveString() {
+        assertEquals("T,false,", emptyTodo.getSaveString());
+    }
+
+    @Test
+    public void toString_emptyDescription_correctString() {
+        assertEquals("[T][\u2718] ", emptyTodo.toString());
+    }
+}
