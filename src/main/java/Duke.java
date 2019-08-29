6817f37a528c4dc84361f5ccc1593010f6e29977
t-cheepeng <tcp97@hotmail.com>
2019-08-30 00:24:39 +0800

    Adds basic GUI functionality to Duke.
    This commit adds up to Tutorial-3 in the JavaFX tutorial for
    adding a GUI to Duke. To run this portion, you are required to
    change your run configurations to have it run the gradle:run task
    instead of the standard java program from IntelliJ.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,10 +8,31 @@ import com.chee.parser.CommandParser;
 import com.chee.io.DukePrinter;
 import com.chee.io.IoUtils;
 import com.chee.io.Input;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.text.ParseException;
+import java.util.NoSuchElementException;
 
-public class Duke {
+public class Duke extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     private TaskList userHistory;
     private DukePrinter dukePrinter;
@@ -30,33 +51,100 @@ public class Duke {
         parser = new CommandParser(userHistory, dukePrinter);
     }
 
-    /**
-     * Initialises and starts up Duke to accept inputs from user.
-     */
-    public void init() {
-        dukePrinter.printWelcome();
-        Command command = null;
-        while (true) {
-            String userInput = input.readInput();
-            try {
-                command = parser.parse(userInput);
-                if (command instanceof ByeCommand) {
-                    command.execute();
-                    break;
-                }
-                command.execute();
-                io.writeTasks(userHistory.getTaskList());
-            } catch (MissingInformationException | UnknownCommandException
-                    | UnknownFormatException | ParseException e) {
-                dukePrinter.printError(e.getMessage());
-            }
-        }
-        input.close();
-        io.close();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+        primaryStage.setTitle("Duke");
+        primaryStage.setResizable(false);
+        primaryStage.setMinHeight(600.0);
+        primaryStage.setMinWidth(400.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        sendButton.setOnMouseClicked((event -> {
+            handleUserInput();
+        }));
+
+        userInput.setOnAction((event -> {
+            handleUserInput();
+        }));
+
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        scene = new Scene(mainLayout);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.init();
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+    /**
+     * Iteration 2:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+        );
+        userInput.clear();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    private String getResponse(String input) {
+        Command command = null;
+        try {
+            command = parser.parse(input);
+            if (command instanceof ByeCommand) {
+                command.execute();
+            }
+            command.execute();
+            io.writeTasks(userHistory.getTaskList());
+            return "Test string";
+        } catch (MissingInformationException | UnknownCommandException
+                | UnknownFormatException | ParseException | NoSuchElementException e) {
+            return e.getMessage();
+        }
     }
 }
