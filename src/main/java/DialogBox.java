ed3ef5266848dc52bc1d68a49700ae43c3afcd84
t-cheepeng <tcp97@hotmail.com>
2019-08-30 20:24:51 +0800

    Add GUI functionality to duke.
    Previously, duke was a purely CLI program which interacted
    with user through text.
    
    With the new GUI functionality, users are now able to view a GUI
    for duke which will help enhance usability and experience for them.
    
    In the future, there is a hope to spiff up the look of the duke
    GUI to make it more appealing to users.

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,41 +1,51 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
+import java.util.Collections;
+
 public class DialogBox extends HBox {
 
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(Label text, ImageView imageView) {
-        this.text = text;
-        this.displayPicture = imageView;
+    private DialogBox(String text, Image imageView) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        text.setWrapText(true);
-        displayPicture.setFitHeight(100.0);
-        displayPicture.setFitWidth(100.0);
-
-        this.setAlignment(Pos.TOP_RIGHT);
-        this.getChildren().addAll(text, displayPicture);
+        dialog.setText(text);
+        displayPicture.setImage(imageView);
     }
 
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
-    public static DialogBox getUserDialog(Label l, ImageView iv) {
+    public static DialogBox getUserDialog(String l, Image iv) {
         return new DialogBox(l, iv);
     }
 
-    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+    public static DialogBox getDukeDialog(String l, Image iv) {
         var db = new DialogBox(l, iv);
         db.flip();
         return db;
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,31 +8,11 @@ import com.chee.parser.CommandParser;
 import com.chee.io.DukePrinter;
 import com.chee.io.IoUtils;
 import com.chee.io.Input;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 import java.text.ParseException;
 import java.util.NoSuchElementException;
 
-public class Duke extends Application {
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+public class Duke {
 
     private TaskList userHistory;
     private DukePrinter dukePrinter;
@@ -51,88 +31,7 @@ public class Duke extends Application {
         parser = new CommandParser(userHistory, dukePrinter);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-        primaryStage.setTitle("Duke");
-        primaryStage.setResizable(false);
-        primaryStage.setMinHeight(600.0);
-        primaryStage.setMinWidth(400.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        sendButton.setOnMouseClicked((event -> {
-            handleUserInput();
-        }));
-
-        userInput.setOnAction((event -> {
-            handleUserInput();
-        }));
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        scene = new Scene(mainLayout);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private Label getDialogLabel(String text) {
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
+    public String getResponse(String input) {
         Command command = null;
         try {
             command = parser.parse(input);
@@ -141,10 +40,15 @@ public class Duke extends Application {
             }
             command.execute();
             io.writeTasks(userHistory.getTaskList());
-            return "Test string";
         } catch (MissingInformationException | UnknownCommandException
                 | UnknownFormatException | ParseException | NoSuchElementException e) {
-            return e.getMessage();
+            dukePrinter.printError(e.getMessage());
         }
+        return dukePrinter.getLatestCommandResponse();
+    }
+
+    public String welcomeUser() {
+        dukePrinter.printWelcome();
+        return dukePrinter.getLatestCommandResponse();
     }
 }
